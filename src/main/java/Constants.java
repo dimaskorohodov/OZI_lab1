e9235578a8aa3b31c8ff0633869a2eb17de519f0
dmsk0320 <dmytro.skorokhodov@netcracker.com>
2020-10-26 23:40:10 +0200

    Refactoring

--- src/main/java/Constants.java
+++
@@ -1,5 +0,0 @@
-public class Constants {
-  public static int FORM_WIDTH = 500;
-  public static int FORM_HEIGHT = 500;
-
-}
--- src/main/java/StartWindow.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class StartWindow {
-  private JTextField textField1;
-
-  private void createUIComponents() {
-    // TODO: place custom component creation code here
-  }
-}
